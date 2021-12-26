3fcf468e1f4ec7db6b3245e698d920b74f4858be
Erna Hulaj <78096644+ernahulaj@users.noreply.github.com>
2021-12-26 19:06:29 +0100

    Update Gallery.js

--- Gallery.js
+++ Gallery.js
@@ -18,3 +18,35 @@ if(galleryImages){
             container.appendChild(newImgWindow);
             newImgWindow.setAttribute("class", "img-window");
             newImgWindow.setAttribute("onclick", "closeImg()");
+
+            let newImg = document.createElement("img");
+            newImgWindow.appendChild(newImg);
+            newImg.setAttribute("src","gallery/"+setNewImgUrl+".jpg");
+            newImg.setAttribute("id","current-img");
+
+            newImg.onload = function(){
+                let imgWidth = this.width;
+                let calcImgToEdge = ((windowWidth - imgWidth)/2) - 90;
+
+                let newNextBtn = document.createElement("a");
+                let btnNextText = document.createTextNode("NEXT");
+                newNextBtn.appendChild(btnNextText);
+                container.appendChild(newNextBtn);
+                newNextBtn.setAttribute("class","img-btn-next");
+                newNextBtn.setAttribute("onclick","changeImg(1)");
+                newNextBtn.style.cssText = "right:"+calcImgToEdge+ "px";
+    
+    
+                let newPrevBtn = document.createElement("a");
+                let btnPrevText = document.createTextNode("PREV");
+                newPrevBtn.appendChild(btnPrevText);
+                container.appendChild(newPrevBtn);
+                newPrevBtn.setAttribute("class","img-btn-prev");
+                newPrevBtn.setAttribute("onclick","changeImg(0)");
+                newPrevBtn.style.cssText = "left:"+calcImgToEdge+ "px";
+            }
+
+            
+        }
+    });
+}
