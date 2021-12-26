89ef44b9e7decac0fedf8469c320586e800d5c10
Erna Hulaj <78096644+ernahulaj@users.noreply.github.com>
2021-12-26 19:05:48 +0100

    Create Gallery.js

---
+++ Gallery.js
@@ -0,0 +1,20 @@
+let galleryImages = document.querySelectorAll(".gallery-img");
+let getLatestOpenedImg;
+let windowWidth = window.innerWidth;
+
+if(galleryImages){
+    galleryImages.forEach(function(image, index) {
+        image.onclick = function(){
+            let getElementCss = window.getComputedStyle(image);
+            let getFullImgUrl = getElementCss.getPropertyValue("background-image");
+            let getImgUrlPos = getFullImgUrl.split("/gallery/thumbs/");
+            let setNewImgUrl = getImgUrlPos[1].replace('.jpeg")','');
+            
+
+            getLatestOpenedImg = index + 1;
+
+            let container = document.body;
+            let newImgWindow = document.createElement("div");
+            container.appendChild(newImgWindow);
+            newImgWindow.setAttribute("class", "img-window");
+            newImgWindow.setAttribute("onclick", "closeImg()");
